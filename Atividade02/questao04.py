749b0f140fc68c0beae5b2443d933b4293617276
BrunoMarinho10 <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>
2025-08-21 09:19:20 -0300

    Atividade02

---
+++ Atividade02/questao04.py
@@ -0,0 +1,9 @@
+distancia = 300
+combustivel = 25
+
+consumo_medio = distancia / combustivel
+
+print("\n=== Calculadora de Consumo de Combustível ===")
+print(f"Distância percorrida: {distancia} km")
+print(f"Combustível gasto: {combustivel} litros")
+print(f"Consumo médio: {consumo_medio:.2f} km/l")
