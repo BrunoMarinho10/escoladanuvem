af8e3db47a211dad6ba97d769f67338fe7f74c5a
BrunoMarinho10 <bmarinho2018@gmail.com>
2025-08-26 09:37:53 -0300

    Atividade Prática 05

---
+++ Atividade05/questao02.py
@@ -0,0 +1,17 @@
+def verifica_palindromo(entrada):
+    
+    entrada_limpa = ''.join(char.lower()
+                            for char in entrada if char.isalnum()
+                            )
+    
+    return "Sim" if entrada_limpa == entrada_limpa[::-1] else "Não"
+
+while True:
+    entrada = input("Digite uma palavra ou frase (ou 'sair' para sair): ")
+    
+    if entrada.lower() == 'sair':
+        print("Programa encerrado.")
+        break 
+    
+    print(f"A palavra ou frase '{entrada}' é um palíndromo? {verifica_palindromo(entrada)}")
+    print()
